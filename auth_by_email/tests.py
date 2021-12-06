4f9a43d186edbfae88976b0730ba163512232550
Alexsik76 <alex@jurist.vn.ua>
2021-12-06 10:40:48 +0200

    work with tests

--- auth_by_email/tests.py
+++ auth_by_email/tests.py
@@ -1,3 +1,33 @@
 from django.test import TestCase
+from django.urls import reverse
+from auth_by_email.models import DjGrammUser
+from django.contrib.auth.models import Permission
+
 
 # Create your tests here.
+
+class SignupViewTest(TestCase):
+    def test_signup(self):
+        response = self.client.get(reverse('signup'))
+        self.assertEqual(response.status_code, 200)
+
+
+class DjUserModelTest(TestCase):
+    def setUp(self) -> None:
+        DjGrammUser.objects._create_user(email='example@email.com',
+                                         bio='mister',
+                                         avatar='picture.png',
+                                         first_name='John',
+                                         last_name='Snow',
+                                         password='password12'
+                                         )
+
+    def test_user_ful_name(self):
+        john = DjGrammUser.objects.get(first_name='John')
+        self.assertEqual(john.get_full_name(), 'John Snow')
+
+    # def test_user_permissions(self):
+    #     user = DjGrammUser.objects.get(first_name='John')
+    #     user.grant_user_permissions()
+    #     self.assertEqual(user.has_perm('auth_by_email.gramm_app.create_post'), True)
+
--- auth_by_email/views.py
+++ auth_by_email/views.py
@@ -18,16 +18,7 @@ from .forms import SignupForm, UserActivationForm, UserUpdateForm
 
 # Create your views here.
 
-from gramm_app.models import Post
-from django.contrib.auth.models import Permission
-from django.contrib.contenttypes.models import ContentType
 
-# content_type = ContentType.objects.get_for_model(Post)
-# permission = Permission.objects.create(
-#     codename='edit_own_post',
-#     name='Edit Own Posts',
-#     content_type=content_type,
-# )
 
 class Signup(View):
     form_class = SignupForm
