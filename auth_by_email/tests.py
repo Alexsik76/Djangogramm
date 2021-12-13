e51c0d2a0802d0a9ab0863ceb8a9afcd1ffa4eaf
Alexsik76 <alex@jurist.vn.ua>
2021-12-13 14:27:12 +0200

    testing 1

--- auth_by_email/tests.py
+++ auth_by_email/tests.py
@@ -11,6 +11,70 @@ class SignupViewTest(TestCase):
         response = self.client.get(reverse('signup'))
         self.assertEqual(response.status_code, 200)
 
+    def test_login(self):
+        response = self.client.get(reverse('login'))
+        self.assertEqual(response.status_code, 200)
+
+
+class AllAuthByEmailViewsTest(TestCase):
+    def setUp(self) -> None:
+        user = DjGrammUser.objects._create_user(id=1,
+                                                email='example@email.com',
+                                                bio='mister',
+                                                avatar='picture.png',
+                                                first_name='John',
+                                                last_name='Snow',
+                                                password='password12')
+        user.grant_user_permissions()
+        self.user_id = 1
+
+    def test_logout(self):
+        c = Client()
+        c.login(email='example@email.com', password='password12')
+        response = c.get(reverse('logout'))
+        self.assertEqual(response.status_code, 200)
+
+    # def test_user_detail(self):
+    #     c = Client(CLOUDINARY_URL='cloudinary://519678674225469:HdXa6q6uJRCtKtvpRN5SM-T1Y-c@dgh6qdngr')
+    #     c.login(email='example@email.com', password='password12')
+    #     response = c.get(reverse('user_detail', args=[1]))
+    #     self.assertEqual(response.status_code, 200)
+    #
+    # def test_user_update(self):
+    #     c = Client(CLOUDINARY_URL='cloudinary://519678674225469:HdXa6q6uJRCtKtvpRN5SM-T1Y-c@dgh6qdngr')
+    #     c.login(email='example@email.com', password='password12')
+    #     response = c.get(reverse('user_update'))
+    #     self.assertEqual(response.status_code, 200)
+
+    def test_password_reset(self):
+        c = Client()
+        c.login(email='example@email.com', password='password12')
+        response = c.get(reverse('password_reset'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_password_reset_done(self):
+        c = Client()
+        c.login(email='example@email.com', password='password12')
+        response = c.get(reverse('password_reset_done'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_password_reset_complete(self):
+        c = Client()
+        c.login(email='example@email.com', password='password12')
+        response = c.get(reverse('password_reset_complete'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_follow(self):
+        c = Client(HTTP_REFERER=reverse('signup'))
+        c.login(email='example@email.com', password='password12')
+        response = c.get(reverse('follow', args=[1]))
+        self.assertEqual(response.status_code, 302)
+
+    def test_unfollow(self):
+        c = Client(HTTP_REFERER=reverse('signup'))
+        c.login(email='example@email.com', password='password12')
+        response = c.get(reverse('unfollow', args=[1]))
+        self.assertEqual(response.status_code, 302)
 
 class DjUserModelTest(TestCase):
     def setUp(self) -> None:
