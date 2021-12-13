9060c3ae3d2c8ff6148b9f55035556a3fd66f1a0
Alexsik76 <alex@jurist.vn.ua>
2021-12-13 15:23:48 +0200

    testing 2

--- auth_by_email/tests.py
+++ auth_by_email/tests.py
@@ -1,10 +1,17 @@
 from django.test import TestCase, Client
 from django.urls import reverse
+from urllib3.util import parse_url
+
 from auth_by_email.models import DjGrammUser
 from gramm_app.models import Post
-
+import random
+import cloudinary
+from cloudinary import CloudinaryResource
+from cloudinary.models import CloudinaryField
 
 # Create your tests here.
+SUFFIX = random.randint(10000, 99999)
+API_TEST_ID = "dj_test_{}".format(SUFFIX)
 
 class SignupViewTest(TestCase):
     def test_signup(self):
@@ -18,10 +25,11 @@ class SignupViewTest(TestCase):
 
 class AllAuthByEmailViewsTest(TestCase):
     def setUp(self) -> None:
+        cloudinary.config(cloud_name='dgh6qdngr')
         user = DjGrammUser.objects._create_user(id=1,
                                                 email='example@email.com',
                                                 bio='mister',
-                                                avatar='picture.png',
+                                                avatar="image/upload/v1234/{}.jpg".format(API_TEST_ID),
                                                 first_name='John',
                                                 last_name='Snow',
                                                 password='password12')
@@ -34,17 +42,29 @@ class AllAuthByEmailViewsTest(TestCase):
         response = c.get(reverse('logout'))
         self.assertEqual(response.status_code, 200)
 
-    # def test_user_detail(self):
-    #     c = Client(CLOUDINARY_URL='cloudinary://519678674225469:HdXa6q6uJRCtKtvpRN5SM-T1Y-c@dgh6qdngr')
-    #     c.login(email='example@email.com', password='password12')
-    #     response = c.get(reverse('user_detail', args=[1]))
-    #     self.assertEqual(response.status_code, 200)
-    #
-    # def test_user_update(self):
-    #     c = Client(CLOUDINARY_URL='cloudinary://519678674225469:HdXa6q6uJRCtKtvpRN5SM-T1Y-c@dgh6qdngr')
-    #     c.login(email='example@email.com', password='password12')
-    #     response = c.get(reverse('user_update'))
-    #     self.assertEqual(response.status_code, 200)
+    def test_to_python(self):
+        c = CloudinaryField('image')
+        res = CloudinaryResource(public_id=API_TEST_ID, format='jpg')
+        # Can't compare the objects, so compare url instead
+        self.assertEqual(c.to_python('{}.jpg'.format(API_TEST_ID)).build_url(), res.build_url())
+
+    def test_user_image_field(self):
+        user = DjGrammUser.objects.get(email='example@email.com')
+        self.assertIsNotNone(user)
+        self.assertEqual(user.avatar.public_id, API_TEST_ID)
+        self.assertTrue(False or user.avatar)
+
+    def test_user_detail(self):
+        c = Client()
+        c.login(email='example@email.com', password='password12')
+        response = c.get(reverse('user_detail', args=[1]))
+        self.assertEqual(response.status_code, 200)
+
+    def test_user_update(self):
+        c = Client()
+        c.login(email='example@email.com', password='password12')
+        response = c.get(reverse('user_update'))
+        self.assertEqual(response.status_code, 200)
 
     def test_password_reset(self):
         c = Client()
@@ -75,6 +95,7 @@ class AllAuthByEmailViewsTest(TestCase):
         c.login(email='example@email.com', password='password12')
         response = c.get(reverse('unfollow', args=[1]))
         self.assertEqual(response.status_code, 302)
+
 
 class DjUserModelTest(TestCase):
     def setUp(self) -> None:
